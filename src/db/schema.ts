7aaa7995addd6fb7bd311aa55be50b1bba5b545d
AkashDeep000 <akashdeepdas970@gmail.com>
2024-04-02 07:32:05 +0530

    bd schema change unique offer

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations, sql } from "drizzle-orm";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer, unique } from "drizzle-orm/sqlite-core";
 import { uid } from "uid";
 
 export const userTable = sqliteTable("user", {
@@ -9,8 +9,10 @@ export const userTable = sqliteTable("user", {
   role: text("role", { enum: ["admin", "user"] })
     .notNull()
     .default("user"),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
-  });
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+});
 
 export type DatabaseUser = typeof userTable.$inferSelect;
 
@@ -27,8 +29,10 @@ export const projectTable = sqliteTable("project", {
   projectId: text("project_id").notNull().unique(),
   affiliateId: integer("affiliate_id").notNull(),
   affiliateName: text("affiliate_name").notNull(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
-  });
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+});
 
 export type DatabaseProject = typeof projectTable.$inferSelect;
 
@@ -36,17 +40,25 @@ export const projectRelations = relations(projectTable, ({ many }) => ({
   offers: many(offerTable),
 }));
 
-export const offerTable = sqliteTable("offer", {
-  id: text("id").notNull().primaryKey().$default(uid),
-  projectId: text("project_id")
-    .notNull()
-    .references(() => projectTable.projectId, { onDelete: "cascade" }),
-  offerId: integer("offer_id").notNull(),
-  offerName: text("offer_name").notNull(),
-  creativeId: integer("creative_id").notNull(),
-  creativeName: text("creative_name").notNull(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
-  });
+export const offerTable = sqliteTable(
+  "offer",
+  {
+    id: text("id").notNull().primaryKey().$default(uid),
+    projectId: text("project_id")
+      .notNull()
+      .references(() => projectTable.projectId, { onDelete: "cascade" }),
+    offerId: integer("offer_id").notNull(),
+    offerName: text("offer_name").notNull(),
+    creativeId: integer("creative_id").notNull(),
+    creativeName: text("creative_name").notNull(),
+    createdAt: text("created_at")
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+  },
+  (t) => ({
+    unq: unique().on(t.projectId, t.offerId, t.creativeId),
+  })
+);
 
 export const offerRelations = relations(offerTable, ({ one }) => ({
   project: one(projectTable, {
